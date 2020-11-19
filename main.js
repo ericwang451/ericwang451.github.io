bbf23f7c66775e7abdc84fd9c0e1f110af4aa178
EC2 Default User <ec2-user@ip-172-31-35-65.us-east-2.compute.internal>
2020-11-19 16:19:59 +0000

    added javascript toggle image feature to profile picture, also added css stuff

--- main.js
+++ main.js
@@ -7,4 +7,14 @@ $(document).ready(function(){
             1000);
     });
 
+    $( "#profile-picture" ).click(function() {
+        
+        const original = "https://media-exp1.licdn.com/dms/image/C5603AQEzj6xm38cJaw/profile-displayphoto-shrink_400_400/0?e=1611187200&v=beta&t=Go5FY4YWhMpmbapZu-54RsBn_h-r8UAc3-NjSAXkIUw";
+        
+        const second = "https://upload.wikimedia.org/wikipedia/en/6/62/Kermit_the_Frog.jpg";
+        
+        const curr = $("#profile-picture").attr("src");
+        
+        $("#profile-picture").attr("src", curr == original ? second : original)
+    });
 });
